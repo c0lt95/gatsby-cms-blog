f06cef502d2ba8317e1417bf411f65757401df51
Ismailov Ibragim (s124306) <ibragim.ismailov@student.ap.be>
2022-02-13 11:41:31 +0100

    logo aanpassen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={blogseshibra} alt="Kaldi" style={{ width: "88px" }} />
+              <img src={blogseshibra} alt="blogseshwithibra" style={{ width: "150px" }} />
             </Link>
             {/* Hamburger menu */}
             <div
